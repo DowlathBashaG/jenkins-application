b6d2b1f22ddc20e97473389eb73e543732612586
DowlathBashaG <dowlath_g@yahoo.com>
2024-05-01 23:11:13 +0530

    second commit

--- src/test/java/io/dowlath/jenkins/application/JenkinsApplicationTests.java
+++ src/test/java/io/dowlath/jenkins/application/JenkinsApplicationTests.java
@@ -13,6 +13,7 @@ class JenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case executing...");
+		logger.info("Test case executing second log statement...");
 		assertEquals(true,true);
 	}
 
